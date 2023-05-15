d0d1d665d969c57ad95dc27d77f7f9a8b7c6a437
Muhammed Shibin <48542917+muhammedshibin@users.noreply.github.com>
2023-05-15 22:59:43 +0400

    Update WeatherForecastController.cs

--- AddressSearch/Controllers/WeatherForecastController.cs
+++ AddressSearch/Controllers/WeatherForecastController.cs
@@ -1,80 +1,47 @@
-public class Worker : BackgroundService
+using IBM.WMQ;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IbmMqListener
 {
-    private bool processMessagePump = true;
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public class MqListenerService : BackgroundService
     {
-        // Create a new thread for the message pump
-        Thread t = new Thread(new ThreadStart(messagePump));
-        t.Start();
+        private readonly ILogger<MqListenerService> _logger;
 
-        // Wait for the worker thread to exit
-        await t.JoinAsync();
-
-        // Close the queue and disconnect from the queue manager
-        queue.Close();
-        qMgr.Disconnect();
-    }
-
-    private void messagePump()
-    {
-        // Create a connection factory object
-        MQConnectionFactory cf = new MQConnectionFactory();
-
-        // Set the properties of the connection factory object
-        cf.HostName = "localhost";
-        cf.Port = 1414;
-        cf.Channel = "SYSTEM.DEF.SVRCONN";
-        cf.QueueManager = "QMGR";
-
-        // Create a connection object
-        MQConnection conn = (MQConnection)cf.CreateConnection();
-
-        // Create a session object
-        MQSession sess = (MQSession)conn.CreateSession(false, AcknowledgeMode.AutoAcknowledge);
-
-        // Create a destination object
-        MQDestination dest = (MQDestination)sess.CreateQueue("QUEUE");
-
-        // Create a consumer object
-        MQConsumer consumer = (MQConsumer)sess.CreateConsumer(dest);
-
-        // Loop forever, blocking on the call to Receive or until a timeout occurs
-        while (processMessagePump)
+        public MqListenerService(ILogger<MqListenerService> logger)
         {
-            try
-            {
-                // Receive the next message from the queue
-                MQMessage msg = (MQMessage)consumer.Receive(5000);
-
-                // Process the message here
-
-                // Reset the message object for reuse
-                msg.ClearMessage();
-            }
-            catch (MQException mqe)
-            {
-                if (mqe.ReasonCode == MQC.MQRC_NO_MSG_AVAILABLE)
-                {
-                    // No messages available - continue looping
-                    continue;
-                }
-                else
-                {
-                    // Some other error occurred - stop looping
-                    processMessagePump = false;
-                    break;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            _logger = logger;
         }
 
-        // Close the consumer, session and connection objects
-        consumer.Close();
-        sess.Close();
-        conn.Close();
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var factory = new MQConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 1414,
+                Channel = "DEV.APP.SVRCONN",
+                QueueManager = "QM1",
+                TransportType = MQC.TRANSPORT_MQSERIES_MANAGED,
+                UserName = "app",
+                Password = "passw0rd"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var session = connection.CreateSession();
+            var queue = session.GetQueue("queue:///DEV.QUEUE.1");
+
+            using var consumer = queue.CreateConsumer();
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var message = consumer.Receive();
+                if (message != null)
+                {
+                    _logger.LogInformation($"Received message: {message.ReadString(message.DataLength)}");
+                    message.Acknowledge();
+                }
+            }
+        }
     }
 }
